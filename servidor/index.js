b42003d7a0b233701e8130a247dba51d87876761
RianBryan12 <ronaldovsouza4@gmail.com>
2024-11-29 11:44:51 -0300

    29/11

--- servidor/index.js
+++ servidor/index.js
@@ -3,6 +3,7 @@ const cors = require('cors');
 const mysql = require('mysql2');
 const app = express();
 const PORT = 3001;
+
 app.use(cors());
 app.use(express.json());
 
@@ -15,24 +16,83 @@ const db = mysql.createConnection({
 });
 
 db.connect((erro) => {
-if (erro) {
-    console.error('Erro ao conectar ao MySQL:', erro);
-}else {
-    console.log('Conectando ao MySQL com sucesso!');
-}
+    if (erro) {
+        console.error('Erro ao conectar ao MySQL:', erro);
+    } else {
+        console.log('Conectado ao MySQL com sucesso!');
+    }
 });
+
 app.post('/alunos', (req, res) => {
-const { nome, cidade, estado } = req.body;
-const sql = 'INSERT INTO alunos (nome, cidade, estado) VALUES (?, ?, ?)';
-db.query(sql,[nome, cidade, estado], (err, result) =>{
-    
-    if (err)
-{
-    return res.status(500).json({ error: 'Erro ao cadastro do aluno!'});
+    const { nome, cidade, estado } = req.body;
 
-}
-res.status(201).json({ messege: 'Aluno cadastrado com sucesso!', id: result.insertId});
-
-res.json(results);
+    const sql = 'INSERT INTO alunos (nome, cidade, estado) VALUES (?, ?, ?)';
+    db.query(sql, [nome, cidade, estado], (err, result) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erro ao cadastrar aluno!'});
+        }
+        res.status(201).json({ message: 'Aluno caddastrado com sucesso!', id: result.insertId});
+    });
 });
+
+app.get('/alunos', (req, res) => {
+    const sql = 'SELECT * FROM alunos';
+
+    db.query(sql, (err, results) => {
+        if (err) {
+            return res.status(500).json({ error: 'Erro ao consultar alunos'});
+        }
+        res.json(results);
+    });
+});
+
+app.get('/alunos/:codigo', (req, res) => {
+    const { codigo } = req.params;
+
+    const sql = 'SELECT * FROM alunos WHERE codigo = ?';
+    db.query(sql, [codigo], (err, results) => {
+        if (err) {
+            console.error('Erro ao buscar usuários:', err);
+            return res.status(500).json({ error: 'Erro ao buscar alunos'});
+        }
+
+        if (results.lenght === 0 ) {
+            console.error('Usuário não encontrado:', err);
+            return res.status(404).json({ error: 'Usuário encontrado'});
+        }
+
+        res.json(results[0]);
+    });
+});
+
+app.put('/alunos/:codigo', (req, res) => {
+    const { codigo } = req.params;
+    const { nome, cidade, estado } = req.body;
+
+    const sql = 'UPDATE alunos SET nome = ?, cidade = ?, estado = ? WHERE codigo = ?';
+
+    db.query(sql, [nome, cidade, estado, codigo], (err) => {
+        if (err) {
+            console.error('Erro ao ayualizar usuário:', err);
+            return res.status(500).json({ error: 'Erro ao atualizar usuário'});
+        }
+        res.json({ message: 'Aluno atualizado com sucesso!'});
+    });
+});
+
+app.delete('/alunos/:codigo', (req, res) =>{
+    const { codigo } = req.params;
+
+    const sql = 'DELETE FROM alunos WHERE codigo = ?';
+
+    db.query(sql, [codigo], (err) =>{
+        if (err) {
+            return res.status(500).json({ error: 'Erro ao excluir aluno'});
+        }
+        res.json({ message: 'Aluno excluído com sucesso!'});
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
 });
